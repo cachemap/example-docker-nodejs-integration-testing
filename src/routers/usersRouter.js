16bc573ea1987c3d391797423dcbcfd9f0c148ac
Dillon <dillon.notz@gmail.com>
2024-02-25 18:05:18 -0500

    Implement POST route; add json body parser

--- src/routers/usersRouter.js
+++ src/routers/usersRouter.js
@@ -1,10 +1,21 @@
-import { Router } from 'express'
+import { Router, json } from 'express'
 import * as usersService from '../services/usersService.js'
 
 const usersRouter = Router()
 
+usersRouter.use(json())
+
 usersRouter.get('/', async (req, res) => {
   res.json(await usersService.getUsers())
+})
+
+usersRouter.post('/', async (req, res) => {
+  const userName = req.body?.userName
+  if (!userName) {
+    return res.status(400).end()
+  }
+
+  res.json(await usersService.createUser({ user_name: userName }))
 })
 
 export default usersRouter
